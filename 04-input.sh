5cb875236bf2dc392aa2d974884ba6e4c4710aa0
Srikanth <srikanth.k1508@gmail.com>
2022-12-28 19:10:54 +0530

    input script

---
+++ 04-input.sh
@@ -0,0 +1,7 @@
+#read -p 'Enter MYSQL Root User Name: ' user
+#read -s -p 'Enter MySQL Root Password: ' pass
+#
+#echo "Username & Password from read = $user & $pass"
+
+echo Username = $1
+echo Password = $2
