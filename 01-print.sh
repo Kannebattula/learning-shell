061a811e1d66498b86dede931ba11fd6ad80b658
Srikanth <srikanth.k1508@gmail.com>
2022-12-28 16:24:52 +0530

    print script

--- 01-print.sh
+++ 01-print.sh
@@ -1,4 +1,9 @@
 echo Helooo
 
 # Color Syntax
-#echo -e "\e["
+# echo -e "\e[COLmHello\e[0m"
+# -e - enable \e
+# COL - COL CODE, 31-36 (31-RED, 32-GREEN, 33 - YELLOW, 34-BLUE, 35-MAGNETA, 36-CYAN)
+# 0M Disable the enabled color
+
+echo -e "\e[31mHello in RED color\e[0m"
